d0d6ad6216aa4d6c788a0e4ae404e16fd88135b8
20it010 <20it010@charusat.edu.in>
2022-09-14 12:32:48 +0530

    first comm

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-
   return (
     <div className="App">
-      <Router>
+      
       <NavigationBar/>
       <Container>
-        <Routes>
-      <Route path="student" element={<Student/>}/>
-      <Route path="listStudents" element={<StudentList/>}/>
-      </Routes>
+      <Student/>
+      <StudentList/>
       </Container>
       <Footer/>
-      </Router>
     </div>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom'
+
 
 export default function NavigationBar() {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link to="/" className='navbar-brand'>Student Management System</Link>
+        <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/student" className="nav-link">Add Student</Link>
-            <Link to="/listStudents" className="nav-link">View Students</Link>
+            <Nav.Link href="#features">Add Student</Nav.Link>
+            <Nav.Link href="#pricing">View Students</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
