0e17901d068afc02ed157a13ca3537b961f26ae8
20it010 <20it010@charusat.edu.in>
2022-09-14 12:38:28 +0530

    first com

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
+import { Card, Container, Table } from "react-bootstrap";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudentList() {
   const [students, setStudents] = useState([]);
@@ -30,7 +28,6 @@ export default function StudentList() {
                 <th>Student Id</th>
                 <th>Student Name</th>
                 <th>Student Address</th>
-                <th>Edit/Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -44,12 +41,6 @@ export default function StudentList() {
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
-                  <td>
-                    <ButtonGroup>
-                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
-                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
-                    </ButtonGroup>
-                  </td>
                 </tr>
                 )
               )}
