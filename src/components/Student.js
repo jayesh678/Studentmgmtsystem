ee0a3e854228bce80f479462d73c48d4f2256346
20it010 <20it010@charusat.edu.in>
2022-09-14 12:17:47 +0530

    first complete

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,66 +1,31 @@
-import axios from "axios";
-import React, {useState} from "react";
+import React from "react";
 import { Container, Form, Card, Button } from "react-bootstrap";
-
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export default function Student() {
-
-  const [id, setId] = useState();
-  const [name, setName] = useState();
-  const [address, setAddress] = useState();
-
-  let student = {
-    id : id,
-    name : name,
-    address : address
-  }
-
-  let textChanged = (event) => {
-    if(event.target.name==="id"){
-      setId(event.target.value);
-    } else if(event.target.name==="name"){
-      setName(event.target.value);
-    } else if(event.target.name==="address"){
-      setAddress(event.target.value);
-    }
-  }
-
-  let saveStudent = (event) => {
-    event.preventDefault();
-    axios.post("http://localhost:8080/student", student)
-    .then(response => {
-      if(response.data != null){
-        alert('Record added successfully');        
-      }
-    })
-    .catch(error => alert(error));
-  }
-
   return (
     <div  className="my-3">
     <Container>
       <Card>
-        <Form onSubmit={saveStudent}>
+        <Form>
           <Card.Header>Add Student Information</Card.Header>
           <Card.Body>
-            <Form.Group className="mb-3">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Id</Form.Label>
-              <Form.Control name="id" value={id} type="text" placeholder="Enter id" onChange={textChanged}/>
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Name</Form.Label>
-              <Form.Control name="name" value={name} type="text" placeholder="Enter name" onChange={textChanged} />
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Address</Form.Label>
-              <Form.Control name="address" value={address} type="text" placeholder="Enter address" onChange={textChanged} />
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
           </Card.Body>
-          <Card.Footer>
           <Button variant="primary" type="submit">
             Submit
           </Button>
-          </Card.Footer>
         </Form>
       </Card>
     </Container>
