90db5e70e7c034740b44b2d61c0296672fd3b4bf
20it010 <20it010@charusat.edu.in>
2022-09-14 12:23:04 +0530

    first done

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,37 +1,74 @@
-import React from 'react'
-import { Container, Table } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
+  };
+
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        alert("Record Deleted Successfully");
+        
+      }
+    })
+  }
+
   return (
     <div className="my-3">
-        <Container>
-        <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Student Id</th>
-          <th>Student Name</th>
-          <th>Student Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-        </Container>
+      <Container>
+        <Card.Header>
+          <h3>Students List</h3>
+        </Card.Header>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+                <th>Edit/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Container>
     </div>
-  )
+  );
 }
