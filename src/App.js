2ed9b73d18a3fb0af0851ac9a0b7809683d7f69f
20it010 <20it010@charusat.edu.in>
2022-09-14 12:10:14 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './components/Student';
+import StudentList from './components/StudentList';
+
+function App() {
+  return (
+    <div className="App">
+      <Student/>
+      <StudentList/>
+    </div>
+  );
+}
+
+export default App;
